9f73747254a50c5d261fa1342793755fb98abec2
Danilova Olga <gladskikh@gmail.com>
2020-09-28 12:22:34 +0700

    Добавила сценарий просмотра фотографий в полноразмерном режиме

--- js/main.js
+++ js/main.js
@@ -1,56 +1,9 @@
 "use strict";
 
-const getRandomNumber = function (min, max) {
-  return min + Math.floor(Math.random() * (max - min + 1));
-};
-
-const getPhotoData = function () {
-  const photoData = [];
-
-  for (let i = 0; i < 25; i++) {
-    const MIN_LIKES = 15;
-    const MAX_LIKES = 200;
-    const url = "photos/" + (i + 1) + ".jpg";
-    const likes = getRandomNumber(MIN_LIKES, MAX_LIKES);
-    const comments = [];
-    const commentsQty = likes / 10;
-
-    for (let j = 0; j < commentsQty; j++) {
-      comments[j] = getComment();
-    }
-
-    photoData[i] = {
-      url: url,
-      description: "описание фотографии",
-      likes: likes,
-      comments: comments.length
-    };
-  }
-  return photoData;
-};
-
-const getComment = function () {
-  const MESSAGES = window.getCommentMessages();
-  const NAMES = window.getName();
-  const avatarNumber = getRandomNumber(1, 6);
-  const name = NAMES[getRandomNumber(0, NAMES.length - 1)];
-  const message = MESSAGES[getRandomNumber(0, MESSAGES.length - 1)];
-  return {
-    avatar: "img/avatar-" + avatarNumber + ".svg",
-    message: message,
-    name: name,
-  };
-};
-
+const photoData = window.getPhotoData();
 const pictures = document.querySelector(".pictures");
 const pictureTemplate = document.querySelector("#picture").content.querySelector(".picture");
-const photoData = getPhotoData();
+const bigPicture = document.querySelector(".big-picture");
 
-for (let i = 0; i < photoData.length; i++) {
-  const picture = pictureTemplate.cloneNode(true);
-  const photoDataItem = photoData[i];
-  picture.querySelector(".picture__img").src = photoDataItem.url;
-  picture.querySelector(".picture__likes").textContent = photoDataItem.likes;
-  picture.querySelector(".picture__comments").textContent = photoDataItem.comments;
-  pictures.appendChild(picture);
-}
+window.placePhotosToPictures(photoData, pictures, pictureTemplate);
+window.showBigPhoto(photoData[0]);
---
+++ js/model.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const getComment = function () {
+  const MESSAGES = window.getCommentMessages();
+  const NAMES = window.getName();
+  const avatarNumber = window.getRandomNumber(1, 6);
+  const name = NAMES[window.getRandomNumber(0, NAMES.length - 1)];
+  const message = MESSAGES[window.getRandomNumber(0, MESSAGES.length - 1)];
+  return {
+    avatar: "img/avatar-" + avatarNumber + ".svg",
+    message: message,
+    name: name,
+  };
+};
+
+window.getPhotoData = function () {
+  const photoData = [];
+
+  for (let i = 0; i < 25; i++) {
+    const MIN_LIKES = 15;
+    const MAX_LIKES = 200;
+    const url = "photos/" + (i + 1) + ".jpg";
+    const likes = window.getRandomNumber(MIN_LIKES, MAX_LIKES);
+    const comments = [];
+    const commentsQty = likes / 10;
+
+    for (let j = 0; j < commentsQty; j++) {
+      comments[j] = getComment();
+    }
+
+    photoData[i] = {
+      url: url,
+      description: "описание фотографии",
+      likes: likes,
+      comments: comments,
+    };
+  }
+  return photoData;
+};
