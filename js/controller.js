9ad1daa0d2ae96618e84b4545838a5f77b857968
Danilova Olga <gladskikh@gmail.com>
2020-10-02 15:23:33 +0700

    Добавила установку фильтра и определение его насыщенности

--- js/controller.js
+++ js/controller.js
@@ -46,11 +46,18 @@ window.showBigPhoto = function (photoNumber) {
 };
 
 // Загрузка фотографии, открытие формы для редактирования
-const uploadPhotoForm = document.querySelector("#upload-select-image");
+const uploadPhotoForm = document.querySelector(".img-upload__form");
 const editPhotoForm = uploadPhotoForm.querySelector(".img-upload__overlay");
 const uploadPhotoButton = uploadPhotoForm.querySelector("#upload-file");
 const closeEditPhotoFormButton = uploadPhotoForm.querySelector(".img-upload__cancel");
 const photoPreview = editPhotoForm.querySelector(".img-upload__preview img");
+
+// Слайдер
+const effectLevelSlider = uploadPhotoForm.querySelector(".img-upload__effect-level");
+const effectLevelLine = effectLevelSlider.querySelector(".effect-level__line");
+const effectLevelDepth = effectLevelSlider.querySelector(".effect-level__depth");
+const effectLevelPin = effectLevelSlider.querySelector(".effect-level__pin");
+const effectLevel = effectLevelSlider.querySelector(".effect-level__value");
 
 // Удаление класса с фильтром
 const removeFilter = function () {
@@ -62,14 +69,50 @@ const removeFilter = function () {
   }
 };
 
-// Выбор фильтра
-const photoFilterChangeHandler = function (evt) {
-  if (evt.target && evt.target.matches(".effects__radio")) {
-    removeFilter();
-    photoPreview.classList.add("effects__preview--" + evt.target.value);
+// Значение насыщенности эффекта фильтра
+const getEffectLevel = function () {
+  const newEffectLevelValue = Math.round((effectLevelDepth.clientWidth / effectLevelLine.clientWidth) * 100);
+  effectLevel.setAttribute("value", newEffectLevelValue.toString());
+};
+
+
+
+// Установить фильтр
+const setFilter = function (filterName) {
+  removeFilter();
+  photoPreview.classList.add("effects__preview--" + filterName);
+  if (filterName === "none") {
+    effectLevelSlider.style.display = "none";
+    photoPreview.style.filter = "";
+  }
+  else {
+    effectLevelSlider.style.display = "block";
+  }
+  getEffectLevel();
+
+  if (filterName === "chrome") {
+    photoPreview.style.filter = "grayscale(" + (effectLevel.value / 100) + ")"
+  }
+  else if (filterName === "sepia") {
+    photoPreview.style.filter = "sepia(" + (effectLevel.value / 100) + ")"
+  }
+  else if (filterName === "marvin") {
+    photoPreview.style.filter = "invert(" + (effectLevel.value) + "%)"
+  }
+  else if (filterName === "phobos") {
+    photoPreview.style.filter = "blur(" + ((effectLevel.value * 3) / 100) + "px)"
+  }
+  else if (filterName === "heat") {
+    photoPreview.style.filter = "brightness(" + ((effectLevel.value * 3) / 100 + 1) + ")"
   }
 };
 
+// Выбор фильтра
+const photoFilterChangeHandler = function (evt) {
+  if (evt.target && evt.target.matches(".effects__radio")) {
+    setFilter(evt.target.value);
+  }
+};
 uploadPhotoForm.addEventListener("change", photoFilterChangeHandler);
 
 
@@ -84,12 +127,17 @@ const openEditPhotoForm = function () {
   editPhotoForm.classList.remove("hidden");
   window.body.classList.add("modal-open");
   window.addEventListener("keydown", inEditPhotoFormEscPress);
+  setFilter("none");
+
+  effectLevelPin.addEventListener("mouseup", function () {
+    console.log("пуньк");
+  });
+  
 };
 const closeEditPhotoForm = function () {
   editPhotoForm.classList.add("hidden");
   window.body.classList.remove("modal-open");
   window.removeEventListener("keydown", inEditPhotoFormEscPress);
-  removeFilter();
 };
 
 uploadPhotoButton.addEventListener("change", function (evt) {
