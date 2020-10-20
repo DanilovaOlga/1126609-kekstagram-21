bb3d2de9f46669944ae39f52e06ff4ef668d0818
Danilova Olga <gladskikh@gmail.com>
2020-10-20 10:24:38 +0700

    Поправила модуль проверки хэштегов

--- js/model/hashtags-validity.js
+++ js/model/hashtags-validity.js
@@ -2,36 +2,52 @@
 
 (function () {
 
-  const checkHashtagsValidity = function () {
-    window.hashtagsString = window.hashtagsString.toLowerCase();
-    let hashtagsArray = window.hashtagsString.split(" ").filter((elem) => elem !== "");
-    window.errorMessage = "";
-    const re = /^#[a-zA-Z0-9]*$/;
-    const MIN_HASHTAG_LENGTH = 2;
-    const MAX_HASHTAG_LENGTH = 20;
-    const MAX_HASHTAG_QUANTITY = 5;
+  const re = /^#[a-zA-Z0-9]*$/;
+  const MIN_HASHTAG_LENGTH = 2;
+  const MAX_HASHTAG_LENGTH = 20;
+  const MAX_HASHTAG_QUANTITY = 5;
+
+  const checkTag = function (hashtag) {
+
+    if (re.test(hashtag) === false) {
+      return "Хэштег должен начинаться с символа # и состоять только из букв и цифр";
+    }
+
+    if (hashtag.length < MIN_HASHTAG_LENGTH) {
+      return "Добавьте буквы или цифры";
+    }
+
+    if (hashtag.length > MAX_HASHTAG_LENGTH) {
+      return "Длина хэштега не должна превышать 20 символов";
+    }
+
+    return "";
+  };
+
+  const checkHashtagsValidity = function (hashtagsString) {
+    hashtagsString = hashtagsString.toLowerCase();
+    let hashtagsArray = hashtagsString.split(" ").filter((elem) => elem !== "");
+
+    if (hashtagsArray.length > MAX_HASHTAG_QUANTITY) {
+      return "Количество хэштегов не должно быть больше 5";
+    }
 
     for (let i = 0; i < hashtagsArray.length; i++) {
       let hashtag = hashtagsArray[i];
 
       for (let j = i + 1; j < hashtagsArray.length; j++) {
-        if (hashtagsArray[i] === hashtagsArray[j]) {
-          window.errorMessage = "Такой хэштег уже есть";
+        if (hashtag === hashtagsArray[j]) {
+          return "Такой хэштег уже есть";
         }
       }
 
-      if (re.test(hashtag) === false) {
-        window.errorMessage = "Хэштег должен начинаться с символа # и состоять только из букв и цифр";
-      } else if (hashtag.length < MIN_HASHTAG_LENGTH) {
-        window.errorMessage = "Добавьте буквы или цифры";
-      } else if (hashtag.length > MAX_HASHTAG_LENGTH) {
-        window.errorMessage = "Длина хэштега не должна превышать 20 символов";
+      const msg = checkTag(hashtag);
+      if (msg) {
+        return msg;
       }
     }
 
-    if (hashtagsArray.length > MAX_HASHTAG_QUANTITY) {
-      window.errorMessage = "Количество хэштегов не должно быть больше 5";
-    }
+    return "";
   };
 
   window.hashtagsValidity = {
